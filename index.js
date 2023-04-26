75da70ee9c4c08054ca427358eb26c081fb2d7b2
derique96 <fowlkes.derique@gmail.com>
2023-04-25 22:12:20 -0400

    continued prompt work

--- index.js
+++ index.js
@@ -6,22 +6,31 @@ inquirer
         type: 'input',
         name: 'text',
         message: 'What text would you like to include (3 Charcters or less)?',
-    })
+    }.then((answer) => {
+        console.log('Answer:', answer.text);
 
-    .prompt({
-        type: 'input',
+    }),
+
+    { 
+         type: 'input',
         name: 'textColor',
         message: 'Which color would you like the text to be?'
-    })
+    }.then((answer) => {
+        console.log('Answer', answer.textColor);
+    }),
 
-    .promp({
+    {
         type: 'input',
         name: 'shape',
         message: 'Please choose a shape.',
-    })
+        choices: ['Triangle', 'Square', 'Circle']
+    }.then((answer) => {
+        console.log('Answer', answer.shape);
+    }),
 
-    .prompt({
-        type: 'input',
-        name:'shapeColor',
-        message: 'Which color would you like the shape to be?'
-    })
+    {  type: 'input',
+    name:'shapeColor',
+    message: 'Which color would you like the shape to be?'
+    }.then((answer) => {
+        console.log('Answer', answer.shapeColor);
+    }));
