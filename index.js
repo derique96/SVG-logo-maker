84b66c69e63f16cffabe4ccb4216574b72997062
derique96 <fowlkes.derique@gmail.com>
2023-04-28 23:50:11 -0400

    prompts are generated

--- index.js
+++ index.js
@@ -2,35 +2,29 @@ const inquirer = require('inquirer');
 const shapes = require('./lib/shapes.js');
 const fs = require('fs/promises');
 inquirer
-    .prompt({
+    .prompt([
+    {
         type: 'input',
         name: 'text',
         message: 'What text would you like to include (3 Charcters or less)?',
-    }.then((answer) => {
-        console.log('Answer:', answer.text);
-
-    }),
-
+    },
     { 
-         type: 'input',
+        type: 'input',
         name: 'textColor',
         message: 'Which color would you like the text to be?'
-    }.then((answer) => {
-        console.log('Answer', answer.textColor);
-    }),
-
+    },
     {
         type: 'input',
         name: 'shape',
         message: 'Please choose a shape.',
         choices: ['Triangle', 'Square', 'Circle']
-    }.then((answer) => {
-        console.log('Answer', answer.shape);
-    }),
+    },
 
     {  type: 'input',
     name:'shapeColor',
     message: 'Which color would you like the shape to be?'
-    }.then((answer) => {
-        console.log('Answer', answer.shapeColor);
-    }));
+    },
+
+    ]).then(answer => {
+        console.log('Answers:',answer);
+    });
