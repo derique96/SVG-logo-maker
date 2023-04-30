9b1bea8655bdcde7fbb0be0f2a60374bf337455f
derique96 <fowlkes.derique@gmail.com>
2023-04-30 18:49:06 -0400

    svg file generated

--- index.js
+++ index.js
@@ -1,30 +1,62 @@
-const inquirer = require('inquirer');
-const shapes = require('./lib/shapes.js');
-const fs = require('fs/promises');
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
+const fs = require("fs");
+
 inquirer
-    .prompt([
+  .prompt([
     {
-        type: 'input',
-        name: 'text',
-        message: 'What text would you like to include (3 Charcters or less)?',
-    },
-    { 
-        type: 'input',
-        name: 'textColor',
-        message: 'Which color would you like the text to be?'
+      type: "input",
+      name: "text",
+      message: "What text would you like to include (3 Charcters or less)?",
     },
     {
-        type: 'input',
-        name: 'shape',
-        message: 'Please choose a shape.',
-        choices: ['Triangle', 'Square', 'Circle']
+      type: "input",
+      name: "textColor",
+      message: "Which color would you like the text to be?",
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: "Please choose a shape.",
+      choices: ["Triangle", "Square", "Circle"],
     },
 
-    {  type: 'input',
-    name:'shapeColor',
-    message: 'Which color would you like the shape to be?'
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "Which color would you like the shape to be?",
     },
-
-    ]).then(answer => {
-        console.log('Answers:',answer);
+  ])
+  .then((answer) => {
+    console.log("Answers:", answer);
+    let shape;
+    switch (answer.shape) {
+      case "Circle":
+        shape = new Circle(answer.shapeColor);
+        break;
+      case "Triangle":
+        shape = new Triangle(answer.shapeColor);
+        break;
+      case "Square":
+        shape = new Square(answer.shapeColor);
+        break;
+    }
+    const solution = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render(answer.shapeColor)}
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${
+          answer.textColor
+        }">${answer.text}</text>
+      </svg>`;
+      const name = "logo.svg";
+      writeFile(name, solution);
+      return solution;
+  });
+  function writeFile(name, solution) {
+    fs.writeFile(name, solution, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("logo created");
+      }
     });
+  }
